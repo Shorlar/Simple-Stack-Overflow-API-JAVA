01fbb91120d7ea15f887141ce4ed5346ffbeb710
Shola <baarbz10@gmail.com>
2023-08-17 12:26:58 +0100

    Created custom query to find questions by users id

--- MyFirstJavaProject/src/main/java/com/stackOverflowAPI/MyFirstJavaProject/DAO/QuestionRepository.java
+++ MyFirstJavaProject/src/main/java/com/stackOverflowAPI/MyFirstJavaProject/DAO/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.stackOverflowAPI.MyFirstJavaProject.DAO;
 
 import com.stackOverflowAPI.MyFirstJavaProject.Entities.Questions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Questions, Integer> {
+    List<Questions> findAllByUsersId(int id);
 }
